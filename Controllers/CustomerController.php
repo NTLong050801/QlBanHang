b481bee87d32ab522ca2808ba49498d799d869fd
minh7721 <nhatminh7721@gmail.com>
2022-02-18 15:14:19 +0700

    update

--- Controllers/CustomerController.php
+++ Controllers/CustomerController.php
@@ -6,14 +6,22 @@ class CusTomerController extends BaseController{
       $this->CustomerModel = new CustomerModel();
   }
     public function index(){
-      $id = $_POST['id'];
-      $nameColID = $_POST['nameCOlID'];
       $getAllLoaiHang = $this -> CustomerModel->getAllLoaiHang();
       $getAllSP = $this -> CustomerModel -> getAllSP();
-      $getSPbyLH = $this -> CustomerModel -> getSPLH($id, $nameColID);
-      return $this-> view('frontend.customer.index',
+      if(isset($_POST['id'])){
+        $id = $_POST['id'];
+        $nameColID = $_POST['nameCOlID'];
+        $getSPbyLH = $this -> CustomerModel -> getSPLH($id, $nameColID);
+        return $this-> view('frontend.customer.index',
+                          ['getAllLoaiHang' => $getAllLoaiHang,
+                          'getAllSP' => $getAllSP, 'getSPbyLH' => $getSPbyLH]);
+      }
+      else{
+        return $this-> view('frontend.customer.index',
                           ['getAllLoaiHang' => $getAllLoaiHang,
                           'getAllSP' => $getAllSP]);
+      }
+   
     }
 
     public function getAllLoaiHang(){
@@ -34,4 +42,3 @@ class CusTomerController extends BaseController{
 
 
 }
-?>
