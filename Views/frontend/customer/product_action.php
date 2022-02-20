f9187c8776aaf6f22cc6d5d1e74fa16ed1ab055a
minh7721 <nhatminh7721@gmail.com>
2022-02-20 16:03:27 +0700

    show Sp theo Loai Hang

--- Views/frontend/customer/product_action.php
+++ Views/frontend/customer/product_action.php
@@ -3,8 +3,9 @@ foreach ($getSPbyLH as $product) {
 ?>
     <div class="col-lg-3 col-md-4 col-sm-6 mix oranges fresh-meat">
         <div class="featured__item">
-            <div class="featured__item__pic set-bg" data-setbg="http://localhost/QlBanHang/public/img/product/<?php echo $product['img'] ?>">
-                <ul class="featured__item__pic__hover">
+            <div class="featured__item__pic" data-setbg="">
+            <img src="http://localhost/QlBanHang/public/img/product/<?php echo $product['img']?>" alt="" class="set-bg">   
+            <ul class="featured__item__pic__hover">
                     <li><a href="#"><i class="fa fa-heart"></i></a></li>
                     <li><a href="#"><i class="fa fa-retweet"></i></a></li>
                     <li><a href="#"><i class="fa fa-shopping-cart"></i></a></li>
